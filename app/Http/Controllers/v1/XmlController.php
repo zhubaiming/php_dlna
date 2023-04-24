8235daa2de35af4880d6783462c96e5b2083d5c4
baiming.zhu <zbm4333@gmail.com>
2023-04-24 21:41:17 +0800

    添加xml转json路由

--- app/Http/Controllers/v1/XmlController.php
+++ app/Http/Controllers/v1/XmlController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class XmlController extends Controller
 {
-    //
+    public function xml2json(Request $request)
+    {
+        $data = $request->post('data');
+
+        $xmlparser = xml_parser_create();
+        xml_parse_into_struct($xmlparser, $data, $result);
+        xml_parser_free($xmlparser);
+
+        dd($result);
+    }
 }
