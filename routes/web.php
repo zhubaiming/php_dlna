fa58f2be0db1392ed49adc2c767f2c59b82978cb
baiming.zhu <zbm4333@gmail.com>
2023-04-25 12:29:00 +0800

    更改开发期间页面显示项目配置信息

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,55 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'title' => '当前运行项目框架配置信息显示',
+        'info' => [
+            'env' => [
+                'file' => file_exists(base_path('.env')) ? '.env文件' : '',
+                'app' => [
+                    'app_name' => env('APP_NAME'),
+                    'app_env' => env('APP_ENV'),
+                    'app_key' => env('APP_KEY'),
+                    'app_debug' => env('APP_DEBUG'),
+                    'app_url' => env('APP_URL')
+                ],
+                'log' => [
+                    'log_channel' => env('LOG_CHANNEL'),
+                    'log_deprecations_channel' => env('LOG_DEPRECATIONS_CHANNEL'),
+                    'log_level' => env('LOG_LEVEL')
+                ],
+                'db' => [
+                    'db_connection' => env('DB_CONNECTION'),
+                    'db_host' => env('DB_HOST'),
+                    'db_port' => env('DB_PORT'),
+                    'db_database' => env('DB_DATABASE'),
+                    'db_username' => env('DB_USERNAME'),
+                    'db_password' => env('DB_PASSWORD')
+                ],
+                'redis' => [
+                    'redis_host' => env('REDIS_HOST'),
+                    'redis_password' => env('REDIS_PASSWORD'),
+                    'redis_port' => env('REDIS_PORT')
+                ],
+            ],
+            'config' => [
+                'app' => [
+                    'file' => 'config/app.php 文件',
+                    'name' => config('app.name'),
+                    'env' => config('app.env'),
+                    'debug' => config('app.debug'),
+                    'url' => config('app.url'),
+                    'asset_url' => config('app.asset_url'),
+                    'timezone' => config('app.timezone'),
+                    'locale' => config('app.locale'),
+                    'fallback_locale' => config('app.fallback_locale'),
+                    'faker_locale' => config('app.faker_locale'),
+                    'key' => config('app.key'),
+                    'cipher' => config('app.cipher'),
+                    'maintenance' => config('app.maintenance')
+                ]
+            ]
+        ]
+    ];
+    return view('welcome', ['data' => $data]);
 });
