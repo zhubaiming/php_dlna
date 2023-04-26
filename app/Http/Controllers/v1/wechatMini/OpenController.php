9711d96a6b7aa4d866537089075c42e7b0d6411f
zhubaiming_Nas <zbm4333@gmail.com>
2023-04-26 06:52:57 +0000

    创建微信小程序【开放接口】控制器

---
+++ app/Http/Controllers/v1/wechatMini/OpenController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\v1\wechatMini;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OpenController extends Controller
+{
+    //
+}
