4e32bf0bbaabd74db85709b74e667fa614cfe77f
baiming.zhu <zbm4333@gmail.com>
2024-01-02 13:53:22 +0800

    定义用户状态枚举

---
+++ app/Enums/UserEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Enums;
+
+class UserEnum
+{
+    // 状态类别
+    public const INVALID = -1; //已删除
+
+    public const NORMAL = 0; //正常
+
+    public const FREEZE = 1; //冻结
+
+    public static function getStatusName($status)
+    {
+        switch ($status) {
+            case self::INVALID:
+                return '已删除';
+            case self::FREEZE:
+                return '冻结';
+            case self::NORMAL:
+                return '正常';
+            default:
+                return '未知';
+        }
+    }
+}
