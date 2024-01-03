d2489da657fd9cac6018d7b7db1afb67e18b1786
baiming.zhu <zbm4333@gmail.com>
2024-01-03 17:53:41 +0800

    基础控制器

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,21 +15,21 @@ class Controller extends BaseController
         date_default_timezone_set("PRC");//这里是添加的时区函数
     }
 
-    protected $offset = 1;
+    protected int $offset = 1;
 
-    protected $limit = 10;
+    protected int $limit = 10;
 
-    protected function setOffset(int $offset)
+    protected function setOffset(int $offset): void
     {
         $this->offset = $offset;
     }
 
-    protected function setLimit(int $limit)
+    protected function setLimit(int $limit): void
     {
         $this->limit = $limit;
     }
 
-    protected function getPageArray()
+    protected function getPageArray(): array
     {
         return [$this->limit, ['*'], 'page', $this->offset];
     }
