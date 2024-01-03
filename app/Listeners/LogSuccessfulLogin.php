edc09ec1e37fe858a5332b0c0c1466d1bec7b749
baiming.zhu <zbm4333@gmail.com>
2024-01-03 17:54:38 +0800

    用户登录成功事件监听器

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\Enums\UserEnum;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redis;
@@ -22,12 +23,17 @@ class LogSuccessfulLogin
     public function handle(Login $event): void
     {
         // 更新用户最后登录时间和登录地址
-        DB::table('log_user_login')->insert([
+        DB::table('log_user_login_infos')->insert([
             'user_id' => $event->user->user_id,
-            'created_at' => date('Y-m-d H:i:s', time()),
-            'ip_address' => app('request')->ip(),
-            'status' => 1,
-            'login_guard' => $event->guard
+            'created_at' => date('Y-m-d H:i:s'),
+            'ip_address' => $event->user->ip,
+            'status' => UserEnum::LOGIN_SUCCESS,
+            'login_guard' => $event->guard,
+            'device_model' => $event->user->device_model ?? null,
+            'device_system' => $event->user->device_system ?? null,
+            'wechat_SDK_version' => $event->user->wechat_SDK_version ?? null,
+            'wechat_language' => $event->user->wechat_language ?? null,
+            'wechat_version' => $event->user->wechat_version ?? null,
         ]);
 
         Redis::del('weixin_login_sms_' . $event->user->country_code . '_' . $event->user->pure_phone_number);
