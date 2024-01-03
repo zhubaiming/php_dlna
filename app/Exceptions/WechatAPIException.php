98eb252659b9bc9049fc6dfd42d99304107de0e5
baiming.zhu <zbm4333@gmail.com>
2024-01-03 17:51:55 +0800

    自定义微信服务端api异常

--- app/Exceptions/WechatAPIException.php
+++ app/Exceptions/WechatAPIException.php
@@ -2,9 +2,27 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Illuminate\Support\Facades\Log;
 
-class WechatAPIException extends Exception
+class WechatAPIException extends BaseException
 {
+    /**
+     * 获取异常上下文信息
+     *
+     * @return array
+     */
+//    public function context(): array
+//    {
+//        return $this->getPrimitives();
+//    }
 
+    /**
+     * 报告或记录异常
+     *
+     * @return void
+     */
+    public function report()
+    {
+        Log::channel('wechatApi')->error('[error] - ' . date('Y-m-d H:i:s') . ' - ' . $this->getCode() . ' - ' . $this->getMessage(), $this->getPrimitives());
+    }
 }
