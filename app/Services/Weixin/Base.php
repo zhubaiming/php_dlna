1c72cc405fc434855cc5d7f390f44e5376737eda
baiming.zhu <zbm4333@gmail.com>
2024-01-03 17:56:36 +0800

    微信服务端获取session_key

--- app/Services/Weixin/Base.php
+++ app/Services/Weixin/Base.php
@@ -2,16 +2,30 @@
 
 namespace App\Services\Weixin;
 
+use App\Services\HttpTrait;
+
 class Base
 {
+    use HttpTrait;
+
     protected $appId;
 
     protected $appSecret;
 
     public function __construct()
     {
-        $this->appId = 'wx3c1e42206a0ecabd';
+        $this->appId = config('services.wechat.mini_app.app_id');
 
-        $this->appSecret = '8c747dbce604aed92c9806863db04f7c';
+        $this->appSecret = config('services.wechat.mini_app.app_secret');
+    }
+
+    public function __call($method, $parameters)
+    {
+        return $this->{$method}(...$parameters);
+    }
+
+    public static function __callStatic(string $func_name, array $args)
+    {
+        return (new static)->{$func_name}(...$args);
     }
 }
--- app/Services/Weixin/MiniApp.php
+++ app/Services/Weixin/MiniApp.php
@@ -2,72 +2,40 @@
 
 namespace App\Services\Weixin;
 
-use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Support\Facades\Http;
+use App\Exceptions\WechatAPIException;
+use Illuminate\Http\Client\RequestException;
 
 class MiniApp extends Base
 {
-    public function jscodeToSession($code)
+    protected function jscodeToSession($code)
     {
-        $url = "https://api.weixin.qq.com/sns/jscode2session?appid={$this->appId}&secret={$this->appSecret}&js_code={$code}&grant_type=authorization_code";
-
         try {
-            $response = Http::withHeaders([])
-                ->connectTimeout(60)
-                ->retry(3, 100)
-                ->asForm()->get('https://api.weixin.qq.com/sns/jscode2session', [
-                    'appid' => $this->appId,
-                    'secret' => $this->appSecret,
-                    'js_code' => $code,
-                    'grant_type' => 'authorization_code'
-                ]);
+            $this->getHttp('https://api.weixin.qq.com/sns/jscode2session', [
+                'appid' => $this->appId,
+                'secret' => $this->appSecret,
+                'js_code' => $code,
+                'grant_type' => 'authorization_code'
+            ]);
 
-            if ($response->successful() && $response->ok()) {
-                return json_decode($response->body(), true);
-            } else {
-//                dump($response->status());
-                dd($response->body());
+            if (isset($this->response_body['errcode'])) {
+                $str = match ($this->response_body['errcode']) {
+                    40163 => 'code已经被使用',
+                    40029 => '无效的code',
+                    40066 => '无效的请求地址',
+                };
+
+                throw new WechatAPIException([
+                    'method_name' => __METHOD__,
+                    'args' => func_get_arg(0)
+                ], $str, $this->response_body['errcode']);
             }
 
-            /**
-             * get 方法返回一个 Illuminate\Http\Client\Response 的实例，该实例提供了大量的方法来检查请求的响应：
-             *
-             * $response->body() : string;
-             * $response->json($key = null, $default = null) : array|mixed;
-             * $response->object() : object;
-             * $response->collect($key = null) : Illuminate\Support\Collection;
-             * $response->status() : int;
-             * $response->successful() : bool;判断状态码是否是 2xx
-             * $response->redirect(): bool;
-             * $response->failed() : bool;判断错误码是否是 4xx或5xx
-             * $response->clientError() : bool;判断错误码是4xx
-             * $response->header($header) : string;
-             * $response->headers() : array;
-             *
-             * Illuminate\Http\Client\Response 对象同样实现了 PHP 的 ArrayAccess 接口，这代表着你可以直接访问响应的 JSON 数据：
-             *
-             * return Http::get('http://example.com/users/1')['name'];
-             * 除了上面列出的响应方法之外，还可以使用以下方法来确定响应是否具有相映的状态码：
-             *
-             * $response->ok() : bool;                  // 200 OK
-             * $response->created() : bool;             // 201 Created
-             * $response->accepted() : bool;            // 202 Accepted
-             * $response->noContent() : bool;           // 204 No Content
-             * $response->movedPermanently() : bool;    // 301 Moved Permanently
-             * $response->found() : bool;               // 302 Found
-             * $response->badRequest() : bool;          // 400 Bad Request
-             * $response->unauthorized() : bool;        // 401 Unauthorized
-             * $response->paymentRequired() : bool;     // 402 Payment Required
-             * $response->forbidden() : bool;           // 403 Forbidden
-             * $response->notFound() : bool;            // 404 Not Found
-             * $response->requestTimeout() : bool;      // 408 Request Timeout
-             * $response->conflict() : bool;            // 409 Conflict
-             * $response->unprocessableEntity() : bool; // 422 Unprocessable Entity
-             * $response->tooManyRequests() : bool;     // 429 Too Many Requests
-             * $response->serverError() : bool;         // 500 Internal Server Error判断错误码是5xx
-             */
-        } catch (ConnectionException $e) {
-            dd($e->getMessage());
+            return json_decode($this->response_body->body(), true);
+        } catch (RequestException $e) {
+            throw new WechatAPIException([
+                'method_name' => __METHOD__,
+                'args' => func_get_arg(0)
+            ], $e->getMessage(), $e->getCode());
         }
     }
 }
