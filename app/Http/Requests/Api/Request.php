acc2da61ff546cd3e3ba6fd7913b54529bbd7cec
baiming.zhu <zbm4333@gmail.com>
2024-01-05 11:32:45 +0800

    API表单验证基类

---
+++ app/Http/Requests/Api/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Request extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
