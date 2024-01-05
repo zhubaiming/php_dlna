5ca76366ce90085fb13846cb19be9aa239a02644
baiming.zhu <zbm4333@gmail.com>
2024-01-05 11:24:34 +0800

    添加登录成功返回函数

--- app/Api/Helpers/ApiResponse.php
+++ app/Api/Helpers/ApiResponse.php
@@ -9,6 +9,8 @@ trait ApiResponse
 {
     protected int $http_code = HttpCode::HTTP_OK;
 
+    protected array $headers = [];
+
 //    protected array $responseHeaders = [];
 //
 //
@@ -17,23 +19,16 @@ trait ApiResponse
 //        return $this->failed($message, HttpCode::HTTP_NOT_FOUND);
 //    }
 //
-//
-//    public function created($message = 'Created')
-//    {
-//        return $this->setStatusCode(HttpCode::HTTP_CREATED)->message($message);
-//    }
 
     /**
-     * 响应成功的接口返回
+     * 登录成功接口
      *
-     * @param $data - 要返回的数据
-     * @param string $status - 返回提示
-     *
+     * @param string $token
      * @return \Illuminate\Http\JsonResponse
      */
-    public function success($data, string $message = 'success'): \Illuminate\Http\JsonResponse // 成功返回数据
+    public function loginSuccess(string $token): \Illuminate\Http\JsonResponse
     {
-        return $this->status(compact('data'), $message); // compact() - 创建一个包含变量名和它们的值的数组
+        return $this->setHeaders(['Authorization' => 'Bearer ' . $token])->message('login success');
     }
 
     /**
@@ -49,23 +44,21 @@ trait ApiResponse
         return $this->setHttpCode($http_code)->message($message, $code);
     }
 
+    private function setHeaders(array $headers): static
+    {
+        $this->headers = array_merge($this->headers, $headers);
+
+        return $this;
+    }
+
     /**
      * @param $message
      * @param string $status
      * @return \Illuminate\Http\JsonResponse
      */
-    private function message($message, $code): \Illuminate\Http\JsonResponse
+    private function message($message, $code = 0): \Illuminate\Http\JsonResponse
     {
-        return $this->status(['message' => $message], $message, $code);
-    }
-
-    /**
-     * @param string $message
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function internalError(string $message = 'Internal Server Error!'): \Illuminate\Http\JsonResponse
-    {
-        return $this->failed($message, 999999, HttpCode::HTTP_INTERNAL_SERVER_ERROR);
+        return $this->status(['message' => $message], $code);
     }
 
     /**
@@ -75,14 +68,10 @@ trait ApiResponse
      * @param int|null $http_code
      * @return \Illuminate\Http\JsonResponse
      */
-    private function status(array $data, $message = null, int $code = 0, int $http_code = null): \Illuminate\Http\JsonResponse
+    private function status(array $data, int $code = 0): \Illuminate\Http\JsonResponse
     {
-        if (!is_null($http_code)) {
-            $this->setHttpCode($http_code);
-        }
-
         $status = [
-            'message' => $message,
+            'message' => 'success',
             'code' => $code
         ];
 
@@ -95,9 +84,9 @@ trait ApiResponse
      * @param array $header
      * @return \Illuminate\Http\JsonResponse
      */
-    private function respond($data, array $header = []): \Illuminate\Http\JsonResponse
+    private function respond($data): \Illuminate\Http\JsonResponse
     {
-        return Response::json($data, $this->getHttpCode(), $header);
+        return Response::json($data, $this->getHttpCode(), $this->getHeaders());
     }
 
     /**
@@ -117,6 +106,43 @@ trait ApiResponse
     private function getHttpCode(): mixed
     {
         return $this->http_code;
+    }
+
+    private function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function created(string $message = 'success'): \Illuminate\Http\JsonResponse
+    {
+        return $this->setHttpCode(HttpCode::HTTP_CREATED)->message($message);
+    }
+
+    /**
+     * 响应成功的接口返回
+     *
+     * @param $data - 要返回的数据
+     * @param string $status - 返回提示
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function success($data, string $message = 'success'): \Illuminate\Http\JsonResponse // 成功返回数据
+    {
+        return $this->status(compact('data'), $message); // compact() - 创建一个包含变量名和它们的值的数组
+    }
+
+
+    /**
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function internalError(string $message = 'Internal Server Error!'): \Illuminate\Http\JsonResponse
+    {
+        return $this->failed($message, 999999, HttpCode::HTTP_INTERNAL_SERVER_ERROR);
     }
 
 
