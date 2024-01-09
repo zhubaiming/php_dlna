6d56f6e03c31c7d610c5d01f476c3a776d6d74a9
baiming.zhu <zbm4333@gmail.com>
2024-01-09 21:08:08 +0800

    调整阿里云短信下发参数

--- app/Services/Aliyun/ShortMessage.php
+++ app/Services/Aliyun/ShortMessage.php
@@ -4,11 +4,11 @@ namespace App\Services\Aliyun;
 
 class ShortMessage extends Base
 {
-    public function sendLoginCode(string $phone, array $template_param): void
+    public function sendLoginCode(string $prefix, string $phone, array $template_param): void
     {
         $this->setTemplateCode('login_template_code');
 
-        $this->assembleSendSmsRequest($phone, $template_param);
+        $this->assembleSendSmsRequest($prefix . $phone, $template_param);
 
         $this->assembleSendSmsWithOptions();
     }
