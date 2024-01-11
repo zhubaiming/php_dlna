64bff5a6fdb26b5b82fb83b1ae31777d3b6eb56c
baiming.zhu <zbm4333@gmail.com>
2024-01-11 15:12:47 +0800

    调整媒体类型模型

--- app/Models/MediaCate.php
+++ app/Models/MediaCate.php
@@ -7,18 +7,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class MediaCate extends Model
 {
-    protected $table = 'cate';
+    protected $table = 'media_cates';
+
+    public $timestamps = false;
+
+    protected $attributes = [
+        'pid' => 0
+    ];
 
     // 匿名全局作用域
     protected static function booted()
     {
         static::addGlobalScope('order_by', function (Builder $builder) {
-            $builder->with(['withChild'])->orderBy('sort', 'asc');
+            $builder->orderBy('sort', 'asc');
+//            $builder->with(['withChild'])->orderBy('sort', 'asc');
         });
     }
 
-    // 关联
-    public function withChild()
+    /*
+     * 局部作用域
+     */
+
+    // 只查询公共和视频
+    public function scopeVideo(Builder $query)
+    {
+        $query->whereIn('belonging_to', ['0', '1']);
+    }
+
+    // 只查询父级分类
+    public function scopeParent(Builder $query): void
+    {
+        $query->where(['pid' => 0]);
+    }
+
+    /*
+     * 关联
+     */
+    public function withManyChild()
     {
         return $this->hasMany(self::class, 'pid', 'id');
     }
