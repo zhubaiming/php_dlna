e8897e33ff475d7e7e1d504d829c80387ad9b06c
baiming.zhu <zbm4333@gmail.com>
2024-01-11 15:11:50 +0800

    修改媒体类型资源

--- app/Http/Resources/Api/MediaCateResource.php
+++ app/Http/Resources/Api/MediaCateResource.php
@@ -14,10 +14,22 @@ class MediaCateResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'child' => self::collection($this->withChild)
-        ];
+        if ($request->is('*/wechatMiniApp/*')) {
+            $return = [
+                'id' => $this->id,
+                'name'=>$this->name,
+                'child'=>self::collection($this->withManyChild)
+            ];
+        }
+
+        return $return;
+
+
+//        dd($request->is('*/wechatMiniApp/*'), $request->path(), $request, $request->route(), $request->getUri());
+//        return [
+//            'id' => $this->id,
+//            'name' => $this->name,
+//            'child' => self::collection($this->withChild)
+//        ];
     }
 }
